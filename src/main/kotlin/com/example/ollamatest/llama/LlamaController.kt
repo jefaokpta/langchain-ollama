ec68583b4a3ec542b3d48da4758c94fa0eb69612
jefaokpta <jefaokpta@hotmail.com>
2024-06-26 16:45:13 -0300

    organizando a casa

---
+++ src/main/kotlin/com/example/ollamatest/llama/LlamaController.kt
@@ -0,0 +1,41 @@
+package com.example.ollamatest.llama
+
+import com.example.ollamatest.config.StructuredPrompt
+import com.example.ollamatest.model.Answer
+import com.example.ollamatest.model.Department
+import com.example.ollamatest.model.Question
+import dev.langchain4j.model.input.structured.StructuredPromptProcessor
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * @author Jefferson Alves Reis (jefaokpta) < jefaokpta@hotmail.com >
+ * Date: 26/06/2024
+ */
+@RestController
+@RequestMapping("/llama")
+class LlamaController(private val llamaService: LlamaService) {
+
+    @PostMapping("/department")
+    fun structuredPrompt(@RequestBody text: String): String{
+        val deptos = listOf(
+//            Department(2, "Financeiro"),
+//            Department(1, "RH"),
+//            Department(3, "Comercial"),
+//            Department(4, "Marketing"),
+//            Department(5, "Suporte")
+            Department(1, "Já sou cliente"),
+            Department(2, "Não sou cliente"),
+        )
+        val deptoTemplate = StructuredPrompt.DepartmentTemplate(text, deptos.map(Department::department))
+        val prompt = StructuredPromptProcessor.toPrompt(deptoTemplate)
+        return llamaService.getOllamaModel().generate(prompt.text())
+    }
+
+    @PostMapping("/chat")
+    fun ragQuestion(@RequestBody question: Question): Answer {
+        return Answer(llamaService.assistant().answer(question.question))
+    }
+}
--- src/main/kotlin/com/example/ollamatest/llama/LlamaService.kt
+++ src/main/kotlin/com/example/ollamatest/llama/LlamaService.kt
@@ -1,6 +1,6 @@
-package com.example.ollamatest.config
+package com.example.ollamatest.llama
 
-import com.example.ollamatest.tools.AssistantSupport
+import com.example.ollamatest.config.Assistant
 import com.example.ollamatest.tools.BookingTool
 import dev.langchain4j.data.document.loader.FileSystemDocumentLoader
 import dev.langchain4j.data.segment.TextSegment
@@ -21,7 +21,7 @@ import java.time.Duration
  * Date: 08/05/2024
  */
 @Service
-class IAConfiguration(private val bookingTool: BookingTool) {
+class LlamaService(private val bookingTool: BookingTool) {
 
     @Value("\${ollama.url}")
     private lateinit var ollamaUrl: String
@@ -29,55 +29,32 @@ class IAConfiguration(private val bookingTool: BookingTool) {
     @Value("\${documents.path}")
     private lateinit var documentsPath: String
 
-    private var ollamaChatModel: OllamaChatModel? = null
 
-    private var assistant: Assistant? = null
-    private var assistantSupport: AssistantSupport? = null
+    private val assistant: Assistant by lazy { createAssistant() }
 
-    fun getIa(): OllamaChatModel {
-        if(ollamaChatModel == null){
-            ollamaChatModel = OllamaChatModelBuilder()
+    fun assistant(): Assistant {
+        return assistant
+    }
+
+    fun getOllamaModel(): OllamaChatModel {
+        return OllamaChatModelBuilder()
                 .baseUrl(ollamaUrl)
                 .modelName("llama3")
                 .temperature(0.0)
                 .timeout(Duration.ofMinutes(1))
                 .build()
-        }
-        return ollamaChatModel!!
     }
 
-    fun getAssistant(): Assistant {
-        if (assistant == null) {
-            assistant = createAssistant()
-        }
-        return assistant!!
-    }
-
-    fun getAssistantSupport(): AssistantSupport {
-        if (assistantSupport == null) {
-            assistantSupport = createAssistantSupport()
-        }
-        return assistantSupport!!
-    }
-
-    fun createAssistant(): Assistant {
+    private fun createAssistant(): Assistant {
         return AiServices.builder(Assistant::class.java)
-            .chatLanguageModel(getIa())
+            .chatLanguageModel(getOllamaModel())
             .chatMemory(MessageWindowChatMemory.withMaxMessages(10))
             .contentRetriever(createContentRetriever())
             .build()
     }
 
-    fun createAssistantSupport(): AssistantSupport {
-        return AiServices.builder(AssistantSupport::class.java)
-            .chatLanguageModel(getIa())
-            .chatMemory(MessageWindowChatMemory.withMaxMessages(10))
-            .tools(bookingTool)
-            .contentRetriever(createTermsOfUse())
-            .build()
-    }
 
-    fun createContentRetriever(): ContentRetriever {
+    private fun createContentRetriever(): ContentRetriever {
         val inMemoryEmbeddingStore: InMemoryEmbeddingStore<TextSegment> = InMemoryEmbeddingStore<TextSegment>()
         val documents = FileSystemDocumentLoader.loadDocuments(documentsPath)
         println("Documents loaded: ${documents.size}")
@@ -85,11 +62,4 @@ class IAConfiguration(private val bookingTool: BookingTool) {
         return EmbeddingStoreContentRetriever.from(inMemoryEmbeddingStore)
     }
 
-    fun createTermsOfUse(): ContentRetriever {
-        val inMemoryEmbeddingStore: InMemoryEmbeddingStore<TextSegment> = InMemoryEmbeddingStore<TextSegment>()
-        val document = FileSystemDocumentLoader.loadDocument("src/main/resources/miles-of-smiles-terms-of-use.txt")
-        println("Document loaded: ${document.text()}")
-        EmbeddingStoreIngestor.ingest(document, inMemoryEmbeddingStore)
-        return EmbeddingStoreContentRetriever.from(inMemoryEmbeddingStore)
-    }
 }
