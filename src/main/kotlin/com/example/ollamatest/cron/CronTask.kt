2d11dd0ebdd3252c6b9888642bfc65b8e1a4a0dc
jefaokpta <jefaokpta@hotmail.com>
2024-08-15 12:31:20 -0300

    keep alive do LLM pq qnd ele hiberna as vzs falha no wake up

--- src/main/kotlin/com/example/ollamatest/cron/CronTask.kt
+++ src/main/kotlin/com/example/ollamatest/cron/CronTask.kt
@@ -1,6 +1,9 @@
 package com.example.ollamatest.cron
 
 import com.example.ollamatest.cache.AssistantCache
+import com.example.ollamatest.llama.LlamaService
+import com.example.ollamatest.model.DepartmentQuestion
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.Scheduled
@@ -11,10 +14,21 @@ import org.springframework.scheduling.annotation.Scheduled
  */
 @Configuration
 @EnableScheduling
-class CronTask(private val assistantCache: AssistantCache) {
+class CronTask(
+    private val assistantCache: AssistantCache,
+    private val llamaService: LlamaService
+) {
+
+    private val log = LoggerFactory.getLogger(this::class.java)
 
     @Scheduled(cron = "0 0 3 * * *")
     fun cleanCacheEveryNight(){
         assistantCache.clear()
+    }
+
+    @Scheduled(fixedRate = 300_000, initialDelay = 120_000)
+    fun keepLLMAliveEvery5Minutes(){
+        log.info("Perguntando a cada 5 minutos")
+        llamaService.classifierDepartment(DepartmentQuestion("Mantendo LLM ativa pra nao crashar", listOf()))
     }
 }
