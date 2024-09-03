81af7a2112b7545149c8da2195dff8fcb8481229
jefaokpta <jefaokpta@hotmail.com>
2024-09-03 12:52:42 -0300

    melhor prompt pra VIP

--- src/main/kotlin/com/example/ollamatest/config/StructuredPrompt.kt
+++ src/main/kotlin/com/example/ollamatest/config/StructuredPrompt.kt
@@ -10,9 +10,11 @@ import dev.langchain4j.model.input.structured.StructuredPrompt
 class StructuredPrompt(prompt: String) {
 
     @StructuredPrompt(
-        "Voçê é a atendente de uma empresa de serviços online e deve adivinhar qual o departamento que melhor atenda a necessidade do cliente: {{text}}",
-        "Responda apenas o número correspondente do departamento: {{departments}}",
+//        "Voçê é atendente de uma empresa de serviços online e deve adivinhar qual o departamento que melhor atenda a necessidade do cliente: {{text}}",
+        "Voçê é atendente de uma empresa de serviços de telefonia",
+        "Descobra se a conversa é de uma que já é cliente ou ainda não é cliente: {{text}}",
+        "Responda apenas o número correspondente da opção: {{options}}",
         "Caso não saiba o que responder, responda apenas o múmero: 0"
     )
-    class DepartmentTemplate(val text: String, val departments: List<String>)
+    class DepartmentTemplate(val text: String, val options: List<String>)
 }
